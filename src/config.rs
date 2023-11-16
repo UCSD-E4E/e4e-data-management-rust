023928b9e60c5361f3b065a7fdae3ab1c83c806f
Chris Cha <ccha@ucsd.edu>
2023-11-15 21:56:41 -0800

    modify behavior to check for existing E4EDMConfig if not, build new config and load it

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,7 @@ use crate::dataset::Dataset;
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::path::PathBuf;
 use std::fs;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -26,13 +27,22 @@ impl E4EDMConfig {
     }
 
     pub fn build() -> Result<E4EDMConfig> {
-        Ok(E4EDMConfig {
-            config_path: std::path::PathBuf::new().join("."),
-            active_dataset: None,
-            active_mission: None,
-            datasets: HashMap::new(),
-            version: VERSION.to_string(),
-            dataset_dir: None,
-        })
+        let config_path = PathBuf::from(".");
+        let config_file_path = config_path.join("config.toml");
+
+        if config_file_path.exists() {
+            let config_str = fs::read_to_string(config_file_path)?;
+            let config: E4EDMConfig = toml::from_str(&config_str)?;
+            Ok(config)
+        } else {
+            Ok(E4EDMConfig {
+                config_path,
+                active_dataset: None,
+                active_mission: None,
+                datasets: HashMap::new(),
+                version: VERSION.to_string(),
+                dataset_dir: None,
+            })
+        }
     }
 }
