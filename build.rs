f40e204a9581160aedfdf692a5ac2e8793311664
Falafel72 <kylehu2000@gmail.com>
2024-01-03 17:42:56 -0800

    fix: using ? operator in build.rs instead of manually unwrapping

--- build.rs
+++ build.rs
@@ -4,8 +4,8 @@ use std::{
 
 fn main() -> Result<(), Box<dyn Error>> {
 
-    let git_output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
-    let git_hash = String::from_utf8(git_output.stdout).unwrap();
+    let git_output = Command::new("git").args(&["rev-parse", "HEAD"]).output()?;
+    let git_hash = String::from_utf8(git_output.stdout)?;
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
     println!("cargo:rustc-env=COMPILE_TIME={}", chrono::Local::now());
     Ok(())
