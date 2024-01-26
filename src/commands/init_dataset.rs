4d96c6a6217d2233cf4d3412c1c987199776dcc6
Aaron Yu <aaronyu330@gmail.com>
2024-01-25 21:53:21 -0800

    fix: Correct Dataset and Manifest root paths (#18)
    * Correct root paths
    
    * Fix review comments

--- src/commands/init_dataset.rs
+++ src/commands/init_dataset.rs
@@ -6,6 +6,9 @@ use directories::ProjectDirs;
 use super::InitDatasetArgs;
 
 pub(crate) fn init_dataset(args: &InitDatasetArgs, config: &mut E4EDMConfig) -> Result<()> {
+    let manifest_root = args.path.to_owned()
+        .unwrap_or(ProjectDirs::from("edu", "UCSD Engineers For Exploration", "E4EDataManagement").unwrap().config_dir().to_path_buf());
+
     let dataset_name = format!("{year:04}.{month:02}.{day:02}.{project}.{location}", 
         year = args.date.year(),
         month = args.date.month(),
@@ -13,14 +16,12 @@ pub(crate) fn init_dataset(args: &InitDatasetArgs, config: &mut E4EDMConfig) -> 
         project = args.project,
         location = args.location
     );
-    let dataset_path = args.path.to_owned()
-        .unwrap_or(ProjectDirs::from("com", "Engineers For Exploration", "E4EDataManagement").unwrap().config_dir().to_path_buf())
-        .join(dataset_name.clone());
 
     if config.datasets.contains_key(&dataset_name) {
         bail!("Dataset with that name already exists!");
     }
+
     config.active_dataset = Some(dataset_name.clone());
-    config.datasets.insert(dataset_name, build_dataset(dataset_path, args.date.with_timezone(&Utc)));
+    config.datasets.insert(dataset_name.clone(), build_dataset(dataset_name, manifest_root, args.date.with_timezone(&Utc)));
     Ok(())
 }
